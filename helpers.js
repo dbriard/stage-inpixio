4b9c521f13aea992389039ad41f41920e18456a3
David BRIARD <dbriard@avanquest.com>
2023-05-16 14:42:31 +0200

    Update

--- helpers.js
+++ helpers.js
@@ -5,7 +5,7 @@
  * @param {Element} [scope] Optional scope element for the selector
  */
 export function qs(selector, scope) {
-	return (scope || document).querySelector(selector);
+    return (scope || document).querySelector(selector);
 }
 
 /**
@@ -17,7 +17,7 @@ export function qs(selector, scope) {
  * @param {boolean} [capture] Capture the event
  */
 export function $on(target, type, callback, capture) {
-	target.addEventListener(type, callback, !!capture);
+    target.addEventListener(type, callback, !!capture);
 }
 
 /**
@@ -31,58 +31,89 @@ export function $on(target, type, callback, capture) {
  * @param {boolean} [capture] Capture the event
  */
 export function $delegate(target, selector, type, handler, capture) {
-	const dispatchEvent = event => {
-		const targetElement = event.target;
-		const potentialElements = target.querySelectorAll(selector);
-		let i = potentialElements.length;
+    const dispatchEvent = event => {
+        const targetElement = event.target;
+        const potentialElements = target.querySelectorAll(selector);
+        let i = potentialElements.length;
 
-		while (i--) {
-			if (potentialElements[i] === targetElement) {
-				handler.call(targetElement, event);
-				break;
-			}
-		}
-	};
+        while (i--) {
+            if (potentialElements[i] === targetElement) {
+                handler.call(targetElement, event);
+                break;
+            }
+        }
+    };
 
-	$on(target, type, dispatchEvent, !!capture);
+    $on(target, type, dispatchEvent, !!capture);
 }
 
 export function getCurrentUrl() {
-	let url = window.location.href;
-	return url;
+    let url = window.location.href;
+    return url;
 };
 
 export function getToto() {
-	return "toto";
+    return "toto";
 };
 
 export function getLastPastedText() {
-	return latestPaste;
+    return latestPaste;
 };
 
 export function getPromptText(message, placeholder) {
-	return prompt(message, placeholder);
+    return prompt(message, placeholder);
 };
 
 export function isFirefox() {
-	return navigator.userAgent.indexOf("Firefox") != -1;
+    return navigator.userAgent.indexOf("Firefox") != -1;
 }
 
 export function initFirefoxPasteWorkaround() {
-	if (navigator.userAgent.indexOf("Firefox") != -1) {
-		const avaloniaInputElement = document.querySelector("input.avalonia-input-element");
-		avaloniaInputElement.contentEditable = true;
-		//document.onpaste = (e) => {
-		//	e.preventDefault();
-		//	pastedText = (e.clipboardData || window.clipboardData).getData("text");
-		//	console.log("pasted text:");
-		//	console.log(pastedText);
-		//};
-		return true;
-	}
-	return false;
+    if (navigator.userAgent.indexOf("Firefox") != -1) {
+        const avaloniaInputElement = document.querySelector("input.avalonia-input-element");
+        avaloniaInputElement.contentEditable = true;
+        //document.onpaste = (e) => {
+        //	e.preventDefault();
+        //	pastedText = (e.clipboardData || window.clipboardData).getData("text");
+        //	console.log("pasted text:");
+        //	console.log(pastedText);
+        //};
+        return true;
+    }
+    return false;
 }
 
+export async function openFilePicker() {
+    //return img src
+    return new Promise((resolve, reject) => {
+        var input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+        input.onchange = (e) => {
+            var file = e.target.files[0];
+            var reader = new FileReader();
+            reader.onload = (e) => {
+                var img = document.createElement("img");
+                img.src = e.target.result;
+                document.body.appendChild(img);
+                resolve(e.target.result);
+            };
+            reader.readAsDataURL(file);
+        };
+        input.click();
+    });
+}
+
+export function saveFile(byteArray, mimeType, fileName) {
+
+
+    var blob = new Blob([byteArray], { type: mimeType });
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName;
+    link.click();
+   
+}
 //export function () {
 //	readClipboardTextAsync(): Promise < string > {
 //		if(globalThis.navigator.clipboard.readText) {
@@ -93,7 +124,3 @@ export function initFirefoxPasteWorkaround() {
 //	}
 //}
 //}
-
-
-
-
